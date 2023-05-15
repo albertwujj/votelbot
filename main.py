5daddf94968d41c6b31461b3f4a20f38e06cdbe6
Albert Wu <albertwujj@gmail.com>
2023-05-15 16:54:12 -0700

    refactor code into VocodeBotResponder class and get_response class func

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 
 import vocode
 from vocode.turn_based.transcriber.whisper_transcriber import WhisperTranscriber
@@ -8,47 +7,53 @@ from vocode.turn_based.agent.chat_gpt_agent import ChatGPTAgent
 from vocode.turn_based.synthesizer.stream_elements_synthesizer import StreamElementsSynthesizer
 from pydub import AudioSegment
 import logging
-from telegram import Update, InputFile
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+import os
+from typing import Tuple
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-api_key = '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'
-transcriber = WhisperTranscriber(api_key=api_key)
-agent = ChatGPTAgent(api_key=api_key, system_prompt='You are TelegramVoiceHuman, a Telegram human having a voice-to-voice conversation with a user as their friend. Have fun!')
-syntheziser = StreamElementsSynthesizer()
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+class VocodeBotResponder():
+    def __init__(self, transcriber, agent, synthesizer):
+        self.transcriber = transcriber
+        self.agent = agent
+        self.synthesizer = synthesizer
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    def get_response(self, voice_input: AudioSegment) -> Tuple[str, AudioSegment]:
+        transcript = self.transcriber.transcribe(voice_input)
+        agent_response = self.agent.respond(transcript)
+        synth_response = self.synthesizer.synthesize(agent_response)
+        return agent_response, synth_response
 
-async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    userVoice = update.message.voice
-    userVoiceFile = await context.bot.get_file(userVoice.file_id)
-    inputFile = 'uservoice.ogg'
-    outputFile = 'synthvoice.ogg'
-    await userVoiceFile.download_to_drive(inputFile)
-    transcript = transcriber.transcribe(AudioSegment.from_file(inputFile))
-    agent_response = agent.respond(transcript)
-    synth_response = syntheziser.synthesize(agent_response)
-    synth_response.export(out_f=outputFile, format='ogg', codec='libopus')
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=agent_response)
-    await context.bot.send_voice(chat_id=update.effective_chat.id, voice=outputFile)
+    async def handle_telegram_start(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a voice bot, please send voice messages to me!")
+
+    async def handle_telegram_voice(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
+        input_file = 'uservoice.ogg'
+        output_file = 'synthvoice.ogg'
+        user_telegram_voice = await context.bot.get_file(update.message.voice.file_id)
+        await user_telegram_voice.download_to_drive(input_file)
+        user_voice = AudioSegment.from_file(input_file)
+        agent_response, synth_response = self.get_response(user_voice)
+        synth_response.export(out_f=output_file, format='ogg', codec='libopus')
+
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=agent_response)
+        await context.bot.send_voice(chat_id=update.effective_chat.id, voice=output_file)
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('6071356894:AAG_odmjKNDQ96nVQtZkejcnTCzo2QZD4KQ').build()
-    start_handler = CommandHandler('start', start) 
-    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
-    voice_handler = MessageHandler(filters.VOICE, handle_voice)
+    transcriber = WhisperTranscriber()
+    agent = ChatGPTAgent(system_prompt='You are TelegramHuman, a Telegram human having a voice-to-voice conversation with a user as their friend. Have fun!')
+    synthesizer = StreamElementsSynthesizer()
+    voco = VocodeBotResponder(transcriber, agent, synthesizer)
+    application = ApplicationBuilder().token(os.environ['TELEGRAM_BOT_KEY']).build()
+    start_handler = CommandHandler('start', voco.handle_telegram_start) 
+    voice_handler = MessageHandler(filters.VOICE & (~filters.COMMAND), voco.handle_telegram_voice)
     application.add_handler(start_handler)
-    application.add_handler(echo_handler)
     application.add_handler(voice_handler)
-    
+
     application.run_polling()
 
     
