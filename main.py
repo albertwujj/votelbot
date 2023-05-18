b234aa558048402b1be6fb8ea34513255a8691c3
Albert Wu <albertwujj@gmail.com>
2023-05-17 21:27:19 -0700

    added case-insensitive user input and config error checking

--- main.py
+++ main.py
@@ -12,40 +12,63 @@ from vocode.turn_based.synthesizer.coqui_synthesizer import CoquiSynthesizer
 from vocode.turn_based.synthesizer.coqui_tts_synthesizer import CoquiTTSSynthesizer
 from vocode.turn_based.synthesizer.rime_synthesizer import RimeSynthesizer
 
-
 from pydub import AudioSegment
 import logging
 import os
 import subprocess
 import inspect
 from typing import Tuple
+
 logging.basicConfig(
  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
  level=logging.INFO)
-
 ''' CONFIGURATION START '''
+# Required environment variables containing API key: OPENAI_API_KEY, TELEGRAM_BOT_KEY, and your Vocode synthesizers classes corresponding API key variable
+
 # Your chosen synthesizer provider's corresponding Vocode turn_based class instance
 SYNTH = ElevenLabsSynthesizer('mqRQRcVDz2T5TcqF5vGi',
                               api_key=os.environ['ELEVEN_LABS_API_KEY'])
 # An dict of voices you want the user to be able to choose from, for each entry the key=name_displayed_to_user and value=provider_voice_id
+# the voice display name must not contain spaces and two names must not be equivalent on a case-insensitive basis (the user is still shown the capitalization but can select names case-insensitively)
 VOICE_NAME_TO_ID = {
- 'Obama': 'zzHoZ9isaKwC4fEv7Idf',
- 'Biden': 'mqRQRcVDz2T5TcqF5vGi'
+ 'BarackObama': 'zzHoZ9isaKwC4fEv7Idf',
+ 'JoeBiden': 'mqRQRcVDz2T5TcqF5vGi'
 }
 # instructions to the language model responsible for generating response transcript.
-SYSTEM_PROMPT = 'Pretend are a human having a voice-to-voice conversation with a user as their friend. Never mention you are a computer or bot. You are a biological human. Have fun!'
+SYSTEM_PROMPT = 'You are having a voice-to-voice conversation with a user as their friend. Always pretend you are a real human. Do not avoid or decline to answer questions because you are not human. Have fun!'
 ''' CONFIGURATION END '''
 
 
+def assert_correct_names(names):
+	lower_name_set = set()
+	for name in names:
+		assert ' ' not in name, 'Voice display names cannot have spaces!'
+		lower_name = name.lower()
+		assert (
+		 lower_name not in lower_name_set
+		), 'Voice display names cannot have duplicates, this requirement is case-insensitive!'
+		lower_name_set.add(lower_name)
+
+
+assert_correct_names(VOICE_NAME_TO_ID.keys())
+
 # key=synth class, value=attribute that chooses the provider voice
-voice_attr_of = {ElevenLabsSynthesizer: 'voice_id', PlayHtSynthesizer: 'voice', StreamElementsSynthesizer: 'voice', AzureSynthesizer: 'voice_name',
-CoquiSynthesizer: 'voice_id', CoquiTTSSynthesizer: 'speaker', RimeSynthesizer: 'speaker'}
+voice_attr_of = {
+ ElevenLabsSynthesizer: 'voice_id',
+ PlayHtSynthesizer: 'voice',
+ StreamElementsSynthesizer: 'voice',
+ AzureSynthesizer: 'voice_name',
+ CoquiSynthesizer: 'voice_id',
+ CoquiTTSSynthesizer: 'speaker',
+ RimeSynthesizer: 'speaker'
+}
 # check voice_attr_of is correct by asserting all classes have their corresponding value as a parameter in the init function
 for key, value in voice_attr_of.items():
 	assert value in inspect.signature(key.__init__).parameters
 
 
 class VocodeBotResponder():
+
 	def __init__(self, transcriber, agent, synthesizer, voice_name_to_id=None):
 		self.transcriber = transcriber
 		self.agent = agent
@@ -54,7 +77,7 @@ class VocodeBotResponder():
 			self.voice_name_to_id = voice_name_to_id
 		else:
 			voice = getattr(self.synthesizer, voice_attr_of[self.synthesizer])
-			self.voice_name_to_id = {voice:voice}
+			self.voice_name_to_id = {voice: voice}
 
 	def get_response(self, voice_input: AudioSegment) -> Tuple[str, AudioSegment]:
 		transcript = self.transcriber.transcribe(voice_input)
@@ -89,30 +112,40 @@ I'm a voice chatbot, send a voice message to me and I'll send one back!" Use /he
 	async def handle_telegram_select_voice(self, update: Update,
 	                                       context: ContextTypes.DEFAULT_TYPE):
 		if not (context.args):
-			await context.bot.send_message(chat_id=update.effective_chat.id,
-		                               text='You must include a voice name. Use /list to list available voices')
+			await context.bot.send_message(
+			 chat_id=update.effective_chat.id,
+			 text='You must include a voice name. Use /list to list available voices')
 			return
-		voice_name = context.args[0]
-		if voice_name not in VOICE_NAME_TO_ID:
-			await context.bot.send_message(chat_id=update.effective_chat.id,
-		                               text='Sorry, I do not recognize that voice. Use /list to list available voices.')
-			return
-		setattr(self.synthesizer, voice_attr_of[type(self.synthesizer)],
-		        VOICE_NAME_TO_ID[voice_name])
-		await context.bot.send_message(chat_id=update.effective_chat.id,
-		                               text='Voice changed successfully!')
+		selected_voice = context.args[0]
+		for voice_name, voice_id in self.voice_name_to_id.items():
+			if voice_name.lower() == selected_voice.lower():
+				setattr(self.synthesizer, voice_attr_of[type(self.synthesizer)], voice_id)
+				await context.bot.send_message(chat_id=update.effective_chat.id,
+				                               text='Voice changed successfully!')
+				return
+		await context.bot.send_message(
+		 chat_id=update.effective_chat.id,
+		 text=
+		 'Sorry, I do not recognize that voice. Use /list to list available voices.')
 
 	async def handle_telegram_list_voices(self, update: Update,
 	                                      context: ContextTypes.DEFAULT_TYPE):
-		voices = "\n".join(VOICE_NAME_TO_ID.keys())
+		voices = "\n".join(self.voice_name_to_id.keys())
 		await context.bot.send_message(chat_id=update.effective_chat.id,
 		                               text=f'Available voices:\n{voices}')
 
-	async def handle_telegram_who(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-		current_voice_id = getattr(self.synthesizer, voice_attr_of[type(self.synthesizer)])
-		current_voice_name = [name for name, id in VOICE_NAME_TO_ID.items() if id == current_voice_id]
-		current_voice_name = current_voice_name[0] if current_voice_name else 'unknown'
-		await context.bot.send_message(chat_id=update.effective_chat.id, text=f"I am currently '{current_voice_name}'.")
+	async def handle_telegram_who(self, update: Update,
+	                              context: ContextTypes.DEFAULT_TYPE):
+		current_voice_id = getattr(self.synthesizer,
+		                           voice_attr_of[type(self.synthesizer)])
+		current_voice_name = [
+		 name for name, id in self.voice_name_to_id.items() if id == current_voice_id
+		]
+		current_voice_name = current_voice_name[
+		 0] if current_voice_name else 'unknown'
+		await context.bot.send_message(
+		 chat_id=update.effective_chat.id,
+		 text=f"I am currently '{current_voice_name}'.")
 
 	async def handle_telegram_help(self, update: Update,
 	                               context: ContextTypes.DEFAULT_TYPE):
@@ -127,15 +160,20 @@ I'm a voice bot, here to talk with you! Here's what you can do:
 '''
 		await context.bot.send_message(chat_id=update.effective_chat.id,
 		                               text=help_text)
-	async def handle_telegram_unknown_cmd(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-		await context.bot.send_message(chat_id=update.effective_chat.id, text='''
-Sorry, I didn\'t understand that command. Use /help to see available commands''')
 
-	async def handle_telegram_unknown(self, update: Update, context: ContextTypes.DEFAULT_TYPE):
-		await context.bot.send_message(chat_id=update.effective_chat.id, text='''
-Sorry, I only respond to voice messages or commands. Use /help for more information.''')
+	async def handle_telegram_unknown_cmd(self, update: Update,
+	                                      context: ContextTypes.DEFAULT_TYPE):
+		await context.bot.send_message(chat_id=update.effective_chat.id,
+		                               text='''
+Sorry, I didn\'t understand that command. Use /help to see available commands'''
+		                               )
 
-
+	async def handle_telegram_unknown(self, update: Update,
+	                                  context: ContextTypes.DEFAULT_TYPE):
+		await context.bot.send_message(chat_id=update.effective_chat.id,
+		                               text='''
+Sorry, I only respond to voice messages or commands. Use /help for more information.'''
+		                               )
 
 
 if __name__ == '__main__':
@@ -154,8 +192,10 @@ if __name__ == '__main__':
 	list_handler = CommandHandler('list', voco.handle_telegram_list_voices)
 	who_handler = CommandHandler('who', voco.handle_telegram_who)
 	help_handler = CommandHandler('help', voco.handle_telegram_help)
-	unknown_cmd_handler = MessageHandler(filters.COMMAND, voco.handle_telegram_unknown_cmd)
-	unknown_handler = MessageHandler(~filters.COMMAND, voco.handle_telegram_unknown)
+	unknown_cmd_handler = MessageHandler(filters.COMMAND,
+	                                     voco.handle_telegram_unknown_cmd)
+	unknown_handler = MessageHandler(~filters.COMMAND,
+	                                 voco.handle_telegram_unknown)
 
 	application.add_handler(start_handler)
 	application.add_handler(voice_handler)
