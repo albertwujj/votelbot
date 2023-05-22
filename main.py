1d5fce42b547740aa1c98bddd63d5a7be5801b25
Albert Wu <albertwujj@gmail.com>
2023-05-22 16:24:52 -0700

    provide voice name to Agent, track separate Agents for each chat id and voice, optimize by avoiding disk when converting voice

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ from vocode.turn_based.synthesizer.coqui_tts_synthesizer import CoquiTTSSynthesi
 from vocode.turn_based.synthesizer.rime_synthesizer import RimeSynthesizer
 
 from pydub import AudioSegment
+import io
 import logging
 import os
 import subprocess
@@ -22,6 +23,10 @@ from typing import Tuple
 logging.basicConfig(
  format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
  level=logging.INFO)
+
+# Synthesizer class must be one of the following (do not change without adding support):
+supported_classes = [StreamElementsSynthesizer, ElevenLabsSynthesizer, PlayHtSynthesizer, AzureSynthesizer, CoquiSynthesizer, CoquiTTSSynthesizer, RimeSynthesizer]
+
 ''' CONFIGURATION START '''
 # Required environment variables containing API key: OPENAI_API_KEY, TELEGRAM_BOT_KEY, and your Vocode synthesizers classes corresponding API key variable
 
@@ -29,15 +34,16 @@ logging.basicConfig(
 SYNTH = ElevenLabsSynthesizer('mqRQRcVDz2T5TcqF5vGi',
                               api_key=os.environ['ELEVEN_LABS_API_KEY'])
 # An dict of voices you want the user to be able to choose from, for each entry the key=name_displayed_to_user and value=provider_voice_id
-# the voice display name must not contain spaces and two names must not be equivalent on a case-insensitive basis (the user is still shown the capitalization but can select names case-insensitively)
+# the voice display name must not contain spaces and two names must not be equivalent on a case-insensitive basis (the user is still shown the capitalization but can select names case-insensitively). The language model will also be given the display name as part of its identity. 
 VOICE_NAME_TO_ID = {
  'BarackObama': 'zzHoZ9isaKwC4fEv7Idf',
  'JoeBiden': 'mqRQRcVDz2T5TcqF5vGi'
 }
 # instructions to the language model responsible for generating response transcript.
-SYSTEM_PROMPT = 'You are having a voice-to-voice conversation with a user as their friend. Always pretend you are a real human. Do not avoid or decline to answer questions because you are not human. Have fun!'
+SYSTEM_PROMPT = 'You are having a voice-to-voice conversation with a user. Always pretend you are a real human. Do not avoid or decline to answer questions because you are not human.'
 ''' CONFIGURATION END '''
-
+# will be passed the voice display name.
+SYSTEM_PROMPT += 'You are {0}. Act like {0} and identify yourself as {0}.'
 
 def assert_correct_names(names):
 	lower_name_set = set()
@@ -48,8 +54,6 @@ def assert_correct_names(names):
 		 lower_name not in lower_name_set
 		), 'Voice display names cannot have duplicates, this requirement is case-insensitive!'
 		lower_name_set.add(lower_name)
-
-
 assert_correct_names(VOICE_NAME_TO_ID.keys())
 
 # key=synth class, value=attribute that chooses the provider voice
@@ -62,6 +66,9 @@ voice_attr_of = {
  CoquiTTSSynthesizer: 'speaker',
  RimeSynthesizer: 'speaker'
 }
+assert set(voice_attr_of.keys()) == set(supported_classes), 'supported_classes must match the keys of voice_attr_of!'
+
+assert type(SYNTH) in voice_attr_of.keys(), 'Synthesizer class must be one of the supported ones!'
 # check voice_attr_of is correct by asserting all classes have their corresponding value as a parameter in the init function
 for key, value in voice_attr_of.items():
 	assert value in inspect.signature(key.__init__).parameters
@@ -69,21 +76,36 @@ for key, value in voice_attr_of.items():
 
 class VocodeBotResponder():
 
-	def __init__(self, transcriber, agent, synthesizer, voice_name_to_id=None):
+	def __init__(self, transcriber, system_prompt, synthesizer, voice_name_to_id=None):
 		self.transcriber = transcriber
-		self.agent = agent
+		self.system_prompt = system_prompt
 		self.synthesizer = synthesizer
+		self.chat_ids = []
 		if voice_name_to_id:
 			self.voice_name_to_id = voice_name_to_id
 		else:
 			voice = getattr(self.synthesizer, voice_attr_of[self.synthesizer])
 			self.voice_name_to_id = {voice: voice}
+		self.voice_id_to_name = {v_id: v_name for v_name, v_id in self.voice_name_to_id.items()}
+		self.agents = {}
 
-	def get_response(self, voice_input: AudioSegment) -> Tuple[str, AudioSegment]:
+	def get_agent(self, chat_id):
+		if chat_id not in self.agents:
+			self.agents[chat_id] = {}
+			for voice_name in self.voice_name_to_id.keys():
+				self.agents[chat_id][voice_name] = ChatGPTAgent(system_prompt=self.system_prompt.format(voice_name))
+		return self.agents[chat_id][self.get_current_voice()]
+
+	def get_response(self, chat_id, voice_input: AudioSegment) -> Tuple[str, AudioSegment]:
 		transcript = self.transcriber.transcribe(voice_input)
-		agent_response = self.agent.respond(transcript)
+		agent_response = self.get_agent(chat_id).respond(transcript)
 		synth_response = self.synthesizer.synthesize(agent_response)
 		return agent_response, synth_response
+
+	def get_current_voice(self):
+		current_voice_id = getattr(self.synthesizer,
+		                           voice_attr_of[type(self.synthesizer)])
+		return self.voice_id_to_name[current_voice_id]
 
 	async def handle_telegram_start(self, update: Update,
 	                                context: ContextTypes.DEFAULT_TYPE):
@@ -95,19 +117,16 @@ I'm a voice chatbot, send a voice message to me and I'll send one back!" Use /he
 
 	async def handle_telegram_voice(self, update: Update,
 	                                context: ContextTypes.DEFAULT_TYPE):
-		input_file = 'uservoice.ogg'
-		output_file = 'synthvoice.ogg'
-		user_telegram_voice = await context.bot.get_file(update.message.voice.file_id
-		                                                 )
-		await user_telegram_voice.download_to_drive(input_file)
-		user_voice = AudioSegment.from_file(input_file)
-		agent_response, synth_response = self.get_response(user_voice)
-		synth_response.export(out_f=output_file, format='ogg', codec='libopus')
-
+		chat_id = update.effective_chat.id
+		user_telegram_voice = await context.bot.get_file(update.message.voice.file_id)
+		bytes = await user_telegram_voice.download_as_bytearray()
+		user_voice = AudioSegment.from_file(io.BytesIO(bytes), format='ogg', codec='libopus')
+		agent_response, synth_response = self.get_response(chat_id, user_voice)
+		out_voice = io.BytesIO()
+		synth_response.export(out_f=out_voice, format='ogg', codec='libopus')
 		await context.bot.send_message(chat_id=update.effective_chat.id,
 		                               text=agent_response)
-		await context.bot.send_voice(chat_id=update.effective_chat.id,
-		                             voice=output_file)
+		await context.bot.send_voice(chat_id=chat_id, voice=out_voice)
 
 	async def handle_telegram_select_voice(self, update: Update,
 	                                       context: ContextTypes.DEFAULT_TYPE):
@@ -136,13 +155,7 @@ I'm a voice chatbot, send a voice message to me and I'll send one back!" Use /he
 
 	async def handle_telegram_who(self, update: Update,
 	                              context: ContextTypes.DEFAULT_TYPE):
-		current_voice_id = getattr(self.synthesizer,
-		                           voice_attr_of[type(self.synthesizer)])
-		current_voice_name = [
-		 name for name, id in self.voice_name_to_id.items() if id == current_voice_id
-		]
-		current_voice_name = current_voice_name[
-		 0] if current_voice_name else 'unknown'
+		current_voice_name = self.get_current_voice()
 		await context.bot.send_message(
 		 chat_id=update.effective_chat.id,
 		 text=f"I am currently '{current_voice_name}'.")
@@ -178,11 +191,10 @@ Sorry, I only respond to voice messages or commands. Use /help for more informat
 
 if __name__ == '__main__':
 	transcriber = WhisperTranscriber()
-	agent = ChatGPTAgent(system_prompt=SYSTEM_PROMPT)
 	synthesizer = SYNTH
 	if type(synthesizer) == ElevenLabsSynthesizer:
 		subprocess.run(['pip', 'install', 'elevenlabs'])
-	voco = VocodeBotResponder(transcriber, agent, synthesizer, VOICE_NAME_TO_ID)
+	voco = VocodeBotResponder(transcriber, SYSTEM_PROMPT, synthesizer, VOICE_NAME_TO_ID)
 	application = ApplicationBuilder().token(
 	 os.environ['TELEGRAM_BOT_KEY']).build()
 	start_handler = CommandHandler('start', voco.handle_telegram_start)
